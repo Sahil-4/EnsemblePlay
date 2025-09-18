8495368340fe7a6aaee2ca02d45401afdf5b9d2b
Sahil-4 <www.sahil.875020@gmail.com>
2025-09-18 13:03:39 +0530

    setup express app

---
+++ ensemble-play-backend/src/app.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (_: Request, res: Response) => {
+  res
+    .status(200)
+    .send({ statusCode: 200, success: true, message: "server is up and running", data: null });
+});
+
+export default app;
