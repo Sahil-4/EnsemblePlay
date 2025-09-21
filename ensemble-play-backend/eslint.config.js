7b2757d46a5467ec29f14adb6913d4b61af27576
Sahil-4 <www.sahil.875020@gmail.com>
2025-09-21 19:11:12 +0530

    integrete eslint and prettier

---
+++ ensemble-play-backend/eslint.config.js
@@ -0,0 +1,22 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "@typescript-eslint/consistent-type-imports": "error",
+      "prettier/prettier": "error",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+    ignores: ["node_modules/**", "dist/**", "build/**"],
+  },
+];
